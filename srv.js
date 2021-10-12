5809b99846640b36f1e533418186b275669128c6
bebop76 <47028739+bebop76@users.noreply.github.com>
2021-10-12 09:29:06 +0200

    Update srv.js

--- srv.js
+++ srv.js
@@ -1,5 +1,6 @@
 const http = require('http')
-const port = 8080
+//const port = 8080 //non usare su Heroku
+const PORT = process.env.PORT || 3000;
 http.createServer((req,res) => {
     res.writeHead(200,{'Content-Type' : 'text/plain'})
     res.end('Hello world citizen')
